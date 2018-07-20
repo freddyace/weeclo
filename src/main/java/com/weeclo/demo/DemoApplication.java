5cc7085d72f9c96604e68b856d02057b956fceee
freddyace <freddyace1993@gmail.com>
2018-07-20 01:27:15 -0400

    Duplicate user registration prevention

--- src/main/java/com/weeclo/demo/DemoApplication.java
+++ src/main/java/com/weeclo/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.weeclo.demo;
 
+import com.sendgrid.*;
 import com.weeclo.demo.entities.UserEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
 
@@ -11,7 +13,7 @@ import java.sql.Timestamp;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		SpringApplication.run(DemoApplication.class, args);
 		UserEntity userEntity = new UserEntity();
 		userEntity.setAddress1("1 Elm Street");
@@ -22,7 +24,29 @@ public class DemoApplication {
 		userEntity.setEmailAddress("freddyace1993@gmail.com");
 		//userEntity.setSt
 
+		Email from = new Email("TeamWeeClo@weeclo.com");
+		String subject = "Welcome to WeeClo!";
+		Email to = new Email("zsines@gmail.com");
+		Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+		Mail mail = new Mail(from, subject, to, content);
+
+//		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		SendGrid sg= new SendGrid("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");
+		Request request = new Request();
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+		} catch (IOException ex) {
+			throw ex;
+		}
+	}
+
 	}
 
 
-}
+

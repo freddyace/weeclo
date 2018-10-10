08311aa3d8debbf23513d4844bea3cdd09568e4c
freddyace <freddyace1993@gmail.com>
2018-10-10 19:37:58 -0400

    Adding Admin page stuff

--- src/main/java/com/weeclo/demo/Controller.java
+++ src/main/java/com/weeclo/demo/Controller.java
@@ -2,9 +2,15 @@ package com.weeclo.demo;
 
 
 import com.weeclo.demo.entities.UserEntity;
+import com.weeclo.demo.session.Certificate;
+import com.weeclo.demo.session.WeeCloSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +27,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 
 /**
@@ -31,6 +38,7 @@ import java.util.List;
 public class Controller {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+        WeeCloSession weeCloSession;
         @Autowired
         private Encryptor encryptor;
 
@@ -50,10 +58,12 @@ public class Controller {
      * of a WeeClo user being passed into this method contains a duplicate email address. Otherwise,
      * a generic exception is thrown.
      */
+    //TODO:Handle other duplicate entry scenarios, ex: Different email, same username
     @Transactional
     @RequestMapping(value = "/createWeeCloUser", method = RequestMethod.POST)
     public ResponseEntity createWeeCloUser(@RequestBody UserEntity weeCloUser){
         HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
         try{
             List<?> results = em.createQuery("select id from UserEntity where emailAddress = ?")
                     .setParameter(1, weeCloUser.getEmailAddress())
@@ -71,18 +81,18 @@ public class Controller {
                 else{
                     System.out.println("False");
                 }
-                return new ResponseEntity("Success",new HttpHeaders(), HttpStatus.OK);
+                return new ResponseEntity<>("Success",responseHeaders, HttpStatus.OK);
             }
             else{
-                return new ResponseEntity("An account with this email already exists.", HttpStatus.NOT_ACCEPTABLE);
+                return new ResponseEntity<>("An account with this email already exists.", responseHeaders ,HttpStatus.NOT_ACCEPTABLE);
             }
         }
         catch(PersistenceException p){
             p.printStackTrace();
-            return new ResponseEntity("An issue with the database occured. Try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("An issue with the database occured. Try again later.",responseHeaders ,HttpStatus.INTERNAL_SERVER_ERROR);
         }catch (Exception e){
             e.printStackTrace();
-            return new ResponseEntity("An issue has occured. We'll get to the bottom of it.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("An issue has occured. We'll get to the bottom of it.",responseHeaders ,HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
@@ -99,21 +109,26 @@ public class Controller {
      */
     @Transactional
     @RequestMapping(value="/login/credentials/", method = RequestMethod.GET)
-    public ResponseEntity login(@RequestParam(value = "email") String email, @RequestParam(value="password") String password){
+    public ResponseEntity<String> login(@RequestParam(value = "email") String email, @RequestParam(value="password") String password){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "*");
         try{
             List<?> list = em.createQuery("select u from UserEntity u where u.emailAddress=?1")
                     .setParameter(1,email )
                     .getResultList();
             if(list.size()!=1){
-
-                return new ResponseEntity<>("We've ran into a problem! Try again later.",HttpStatus.OK);
+                return new ResponseEntity<>("Incorrect credentials",headers,HttpStatus.FORBIDDEN);
             }
             if(list.size()==1){
+                weeCloSession = new WeeCloSession(new Certificate());
                 UserEntity userEntity = (UserEntity) list.get(0);
-                if(passwordEncoder.matches(password, userEntity.getPassword()))
-                    return new ResponseEntity<>("Successfully validated user!", HttpStatus.OK);
+                if(passwordEncoder.matches(password, userEntity.getPassword())) {
+                    return new ResponseEntity<>("Successfully validated user! Assigned session token ID: "+weeCloSession.getToken().getID()
+                            +"with headers: "+headers, headers, HttpStatus.OK);
+
+                }
                 else
-                    return new ResponseEntity<>("Wrong credentials!", HttpStatus.OK);
+                    return new ResponseEntity<>("We've ran into a problem! Try again later.", headers,HttpStatus.OK);
 
             }
         }catch (PersistenceException p ){
@@ -139,7 +154,10 @@ public class Controller {
 
     @RequestMapping(value = "/uploadFile", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        File convertFile = new File("/Users/freddyacevedo/IdeaProjects/Bars2018/src/main/"+file.getOriginalFilename());
+        Resource resource = new ClassPathResource("");
+        Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+        //properties.getProperty("images.directory");
+        File convertFile = new File(properties.getProperty("images.directory")+file.getOriginalFilename());
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(file.getBytes());
